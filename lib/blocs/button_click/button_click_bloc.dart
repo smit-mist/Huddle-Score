14ef6ccc275580b6b53b3f9aa50dfa967c64cf9e
yashj6294 <56912613+yashj6294@users.noreply.github.com>
2021-08-15 13:23:56 +0530

    minor changes

--- lib/blocs/button_click/button_click_bloc.dart
+++ lib/blocs/button_click/button_click_bloc.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
 part 'button_click_event.dart';
 part 'button_click_state.dart';
