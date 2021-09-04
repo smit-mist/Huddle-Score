fb2128269e788950d477ebf52868e3392898d680
smit-mist <smit3355mistry@gmail.com>
2021-09-04 22:37:13 +0530

    Print Booking Working Properly.

--- lib/screens/auth_and_user/sign_in_screen.dart
+++ lib/screens/auth_and_user/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:huddle_and_score/blocs/login/login_bloc.dart';
 import 'package:huddle_and_score/screens/home_navbar_screen.dart';
@@ -155,6 +156,7 @@ class _SignInScreenState extends State<SignInScreen> {
                                   visible = !visible;
                                 });
                               },
+                              //child: SvgPicture.asset('assets/icons/password_eye.svg',),
                               child: Icon(
                                 (visible == true
                                     ? Icons.visibility
